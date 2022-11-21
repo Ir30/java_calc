240df19cc73a75b51c7f9663169918b47b71b2da
irshad007 <irshadmuhammedch8@gmail.com>
2022-11-21 10:15:07 +0530

    calculator started

---
+++ calc.java
@@ -0,0 +1,12 @@
+class calc{
+
+    public static void main(String[] args) {
+        System.out.println("Choose the operation");
+        System.out.println("---------------------");
+        System.out.println("1:Addition");
+        System.out.println("2:Substraction");
+        System.out.println("3:Multiplication");
+        System.out.println("4:Division");
+        
+    }
+}
